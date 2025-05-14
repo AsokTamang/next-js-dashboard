30d3d21228a331411f8615de1ccc5cf0180aa9c6
AsokTamang <ashoktmg2058@gmail.com>
2025-05-14 16:43:55 -0400

    pushed with correction

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,4 +4,4 @@ import {Inter,Lusitana} from 'next/font/google'
  const inter=Inter({subsets:['latin']})  ; //here we are importing the Inter from google font and using subset as latin which is for western or english language
  const lusitana=Lusitana({subsets:['latin'],weight:'700'});
 
- export {inter,lusitana}
+ export  {inter,lusitana}
